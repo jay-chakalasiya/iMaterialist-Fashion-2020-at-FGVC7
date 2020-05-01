17209d02d33878abc47bd9c6bc764e12db76ff2b
jay-chakalasiya <chakalasiyajay00@gmail.com>
2020-05-01 15:48:45 -0700

    Data loader and Config

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-class Config:
-
-    def __init__ (self):
-        self.IMAGE_SIZE = 256
-        self.NO_OF_CLASSES = 46
-        self.DATA_PATH = 'data/imaterialist-fashion-2020-fgvc7'
+class Config(object):
+    DATA_PATH = 'data/imaterialist-fashion-2020-fgvc7'
+    IMAGE_SIZE = 256
+    NO_OF_CLASSES = 46
+    BATCH_SIZE = 8
+    OUTPUT_MASK_SIZE=256
+    RESIZE=True
+    
